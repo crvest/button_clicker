f910b3dbd7922d9bed3265ee47afcb38a31b2239
crvest <chad.vest@gmail.com>
2022-03-27 15:44:13 -0400

    button_clicker complete

---
+++ script.js
@@ -0,0 +1,14 @@
+// remove element when click
+function hide(element) {
+    element.remove();
+}
+
+// change login button text
+function logout(element) {
+    element.innerText = "log out"
+}
+
+// allert when like button is clicked
+function like(element) {
+    alert("ninja was liked");
+}
